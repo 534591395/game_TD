e240af70c6906c8c1c270577d8c38a269f2d0f1c
hzwangyang1 <hzwangyang1@corp.netease.com>
2019-07-03 16:39:54 +0800

    【MOD】

--- src/Gatling.ts
+++ src/Gatling.ts
@@ -9,7 +9,14 @@ class Gatling extends egret.DisplayObjectContainer {
     private minDamage = 0;
     // 机枪最大伤害值
     private maxDamage = 0;
-
+    // 机枪转动时间（开火）
+    private turnTime = 0;
+    // 机枪每次发射子弹时间（机枪不可能无间断发子弹，总得间隔一下）
+    private fireTime = 0
+    // 当前每帧对应的角度数
+    private currentAngleFrame = 0;
+    // 当前角度数
+    private currentAngle = 0;
 
     // 机枪等级
     public level = 0;
@@ -26,9 +33,79 @@ class Gatling extends egret.DisplayObjectContainer {
     // 实际机枪转速（说明：游戏快进会影响这个速度，故转速做了两个变量区分）
     public realTurnSpeed = 0;
     
+    // 机枪可升级到的最大等级
+    public maxLevel = 10;
+    
+    public tx = -1;
+    public ty = -1;
+
+    public static IDLE = 'idle';
+    public static FIRE = 'fire';
+    // 整体机枪的初始等级，当前全体默认0级
+    public static currentLevel = 0;
+    // 每级别对应的属性，伤害、升级花费、价格、转速、攻击范围
+    public static levels = [{cost:50, sellMoney:25, upgradeMoney:45, minDamage:10, maxDamage:20, attackRadius:135, turnSpeed:300},
+				  {cost:90, sellMoney:45, upgradeMoney:65, minDamage:20, maxDamage:50, attackRadius:160, turnSpeed:300},
+				  {cost:150, sellMoney:75, upgradeMoney:95, minDamage:50, maxDamage:80, attackRadius:200, turnSpeed:300}];
 
 
-    constructor() {
+    public constructor() {
         super();
+    }
+
+    //
+    public static setLevel(target:Gatling, level:number) {
+        target.level = level;
+        target.cost = Gatling.levels[level].cost;
+        target.sellMoney = Gatling.levels[level].sellMoney;
+        target.upgradeMoney = Gatling.levels[level].upgradeMoney;
+        target.minDamage = Gatling.levels[level].minDamage;
+        target.maxDamage = Gatling.levels[level].maxDamage;
+        target.attackRadius = Gatling.levels[level].attackRadius;
+        target.realTurnSpeed = target.turnSpeed = Gatling.levels[level].turnSpeed;
+    }
+
+    public static getLevel(level:number) {
+        if (typeof level === 'undefined') {
+            level = Gatling.currentLevel;
+        }
+        if (level < 0 || level >= Gatling.levels.length) {
+            return null;
+        }
+        return Gatling.levels[level];
+    }               
+    
+    // 判断是否能升级
+    public canUpgrade() {
+        return this.level < this.maxLevel;
+    }
+
+    // 升级
+    public upgrade() {
+        if (!this.canUpgrade()) {
+            return;
+        }
+        this.level++;
+        this.cost = this.cost + this.upgradeMoney;
+        // 卖出的价钱，等于总价值的一半（取整，1.2 == 1, 1.6== 1）
+        this.sellMoney = this.cost * 0.5 >> 0;
+        this.upgradeMoney = this.upgradeMoney + 25;
+        this.minDamage = this.minDamage + 10;
+        this.maxDamage = this.maxDamage + 20 + this.level * this.level;
+        if (this.attackRadius < 200) {
+            this.attackRadius = this.attackRadius + 10;
+        }
+        this.turnSpeed += 2;
+        this.realTurnSpeed +=2;
+    }
+
+    private create() {
+        Gatling.setLevel(this, 0);
+
+        // 动画
+    }
+
+    private getRealFrame(angleFrame, angle) {
+        return angleFrame;
     }
 }
